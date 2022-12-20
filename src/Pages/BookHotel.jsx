3203f429214f3b8f842427b2de3eac7f0a8699b7
santosh <styadav018@gmail.com>
2022-12-20 15:17:38 +0530

    added

--- src/Pages/BookHotel.jsx
+++ src/Pages/BookHotel.jsx
@@ -32,7 +32,7 @@ export const BookHotel = () => {
   const params = useParams();
   // dnsjkfnjsd
 // dsjfohghdfosjdfnsod
-sjgsj
+sjgsjwfhdhf
   useEffect(() => {
     dispatch(getSingleHotelData(params.id)).then((res) => {
       setLoading(false);
